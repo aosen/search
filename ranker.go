4de29ba88389ebfa5c1a48afcc836ffe661a211a
aosen <zhangzhen2012@foxmail.com>
2015-12-18 17:51:36 +0800

    接口话

--- ranker.go
+++ ranker.go
@@ -9,6 +9,18 @@ import (
 	"sync"
 )
 
+//排序起接口
+type SearchRanker interface {
+	//排序起初始化
+	Init()
+	// 给某个文档添加评分字段
+	AddScoringFields(docId uint64, fields interface{})
+	// 删除某个文档的评分字段
+	RemoveScoringFields(docId uint64)
+	// 给文档评分并排序
+	Rank(docs []IndexedDocument, options RankOptions) (outputDocs ScoredDocuments)
+}
+
 type RankOptions struct {
 	// 文档的评分规则，值为nil时使用Engine初始化时设定的规则
 	ScoringCriteria ScoringCriteria
